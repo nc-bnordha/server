a48fc14c9f0ff3fcfaa34582bbc99963a8b2ffee
Bernd Nordhausen <bernd@nordhausenconsulting.com>
2022-05-11 11:42:16 +0800

    .

--- startup.sh
+++ startup.sh
@@ -2,5 +2,5 @@
 apt update
 apt -y install apache2
 cat <<EOF > /var/www/html/index.html
-<html><body><p>Hello World!</p><p>Version 1.0.0</p></body></html>
+<html><body><p>Hello World!</p><p>Version 2.0.0</p></body></html>
 
